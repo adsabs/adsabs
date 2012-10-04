9d08c88cae07a34a23fa40da1bc2c2df82716202
Jay Luker <jluker@cfa.harvard.edu>
2012-10-04 13:25:21 -0400

    method for fetching all DataCollection models

--- adsabs/core/data/mongo/__init__.py
+++ adsabs/core/data/mongo/__init__.py
@@ -1,1 +1,12 @@
 from models import *
+
+import sys
+import inspect
+
+def data_collections():
+    dc = []
+    for name, obj in inspect.getmembers(sys.modules[__name__]):
+        if inspect.isclass(obj) and issubclass(obj, DataCollection):
+            dc.append(obj)
+    return dc
+        
