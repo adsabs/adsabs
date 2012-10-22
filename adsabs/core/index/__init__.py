46951d9cffbbf2adb3b083e1897ecb9c5f7109e5
Giovanni Di Milia <giovanni@dimilia.it>
2012-10-22 14:42:13 -0400

    Unique base search form used both in main page and in search module

--- adsabs/core/index/__init__.py
+++ adsabs/core/index/__init__.py
@@ -1,4 +1,6 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request
+
+from adsabs.modules.search.forms import QueryForm
 
 #I define the blueprint
 index_blueprint = Blueprint('index', __name__)
@@ -6,6 +8,8 @@ index_blueprint = Blueprint('index', __name__)
 @index_blueprint.route('/', methods=['GET', 'POST'])
 def index():
     """
-    
+    Entry point of the web site
     """
-    return render_template('index/main_page.html')
+    search_form = QueryForm(request.values, csrf_enabled=False)
+    
+    return render_template('index/main_page.html', form=search_form)
