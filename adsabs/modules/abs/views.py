6cbec685d07f4ebf5718d8125e0fbfd864f4a4ef
Giovanni Di Milia <giovanni@dimilia.it>
2012-12-27 12:30:55 -0500

    Moved the search macros to the global templates
    (they are useful also for the references, citations and any other list)
    Working on references

--- adsabs/modules/abs/views.py
+++ adsabs/modules/abs/views.py
@@ -16,20 +16,27 @@ __all__ = ['abs_blueprint']
 @abs_blueprint.route('/<bibcode>', methods=['GET'])
 def abstract(bibcode):
     solrdoc = solr.get_document(bibcode)
+    if not solrdoc:
+        abort(404)
     inveniodoc = invenio.get_invenio_metadata(bibcode)
     #I append to the g element a dictionary of functions I need in the template
     g.formatter_funcs = {'field_to_json':field_to_json}
-    if not solrdoc:
-        abort(404)
     return render_template('abstract_tabs.html', solrdoc=solrdoc, inveniodoc=inveniodoc, curview='abstract')
     
 @abs_blueprint.route('/<bibcode>/references', methods=['GET'])
 def references(bibcode):
+    #I get the document
     solrdoc = solr.get_document(bibcode)
-    inveniodoc = invenio.get_invenio_metadata(bibcode)
+    #if there are no references I return a 404
     if not solrdoc or not solrdoc.has_references():
         abort(404)
-    return render_template('abstract_tabs.html', solrdoc=solrdoc, inveniodoc=inveniodoc, curview='references')
+    #I get the additional metadata
+    inveniodoc = invenio.get_invenio_metadata(bibcode)
+    #I get the list of references
+    solr_reference_list = solrdoc.get_references()
+    #I append to the g element a dictionary of functions I need in the template
+    g.formatter_funcs = {'field_to_json':field_to_json}
+    return render_template('abstract_tabs.html', solrdoc=solrdoc, inveniodoc=inveniodoc, curview='references', reference_list=solr_reference_list)
 
 @abs_blueprint.route('/<bibcode>/citations', methods=['GET'])
 def citations(bibcode):
