c05c4a4a84d9563a33deb8a76837ec2ead9c29b3
Jay Luker <jluker@cfa.harvard.edu>
2013-01-04 16:52:21 -0500

    adding sub-manager for api user commands + new commands

---
+++ adsabs/modules/api/manager.py
@@ -0,0 +1,90 @@
+'''
+Created on Jan 4, 2013
+
+@author: jluker
+'''
+
+import sys
+import logging
+import simplejson
+
+from flask.ext.script import Manager, prompt, prompt_choices, prompt_bool #@UnresolvedImport
+from config import config
+
+manager = Manager("Perform api user operations", with_default_commands=False)
+
+log = logging.getLogger(__name__)
+
+@manager.command
+def useradd(email=None, level=None):
+    """add an api user"""
+
+    from adsabs.modules.user import AdsUser
+    from adsabs.modules.api import user
+    
+    if not email:
+        email = prompt("Enter e-mail address of Classic ADS account", "")
+        if not len(email):
+            sys.exit(1)
+    if not level:
+        level = prompt_choices("Enter developer permission level", sorted(user.PERMISSION_LEVELS.keys()), "basic")
+        if level not in user.PERMISSION_LEVELS:
+            sys.exit(1)
+            
+    ads_user = AdsUser.from_email(email)
+    if not ads_user:
+        log.info("user not found")
+        sys.exit(1)
+        
+    # first check if the user is already a dev
+    api_user = user.AdsApiUser(ads_user.user_rec)
+    if api_user.is_developer():
+        log.info("User already has api access. Developer key: %s" % api_user.get_dev_key())
+        if not prompt_bool("Would you like to reset the user's permissions", False):
+            user.set_permissions(level)
+            log.info("API User permissions updated")
+            
+    api_user = user.create_api_user(ads_user, level)
+    dev_key = api_user.get_dev_key()
+    log.info("API User created with %s permissions. Developer key: %s" % (level, dev_key))
+    
+@manager.command
+def userinfo(email=None, dev_key=None):
+    """dump the api-related parts of the mongo user record"""
+    from adsabs.modules.api import user
+    
+    if email:
+        api_user = user.AdsApiUser.from_email(email)
+    elif dev_key:
+        api_user = user.AdsApiUser.from_dev_key()
+    else:
+        log.error("You must provide an email address or dev_key for the lookup")
+        sys.exit(1)
+    
+    if not api_user:
+        log.info("User not found")
+    else:
+        log.info("User dev_key: %s" % api_user.get_dev_key())
+        log.info(simplejson.dumps(api_user.get_dev_perms(), indent=True))
+        
+@manager.command
+def userdel(email=None, dev_key=None):
+    """remove a user's developer api status"""
+    from adsabs.modules.api import user
+    
+    if email:
+        api_user = user.AdsApiUser.from_email(email)
+    elif dev_key:
+        api_user = user.AdsApiUser.from_dev_key()
+    else:
+        log.error("You must provide an email address or dev_key for the lookup")
+        sys.exit(1)
+    
+    if not api_user:
+        log.info("User not found")
+    elif prompt_bool("Remove API access for user %s" % api_user.get_username(), True):
+        api_user.set_dev_key(None)
+        api_user.set_perms(new_perms={})
+        
+    
+    
--- shell.py
+++ shell.py
@@ -7,25 +7,22 @@ import tempfile
 import subprocess
 import logging
 
-from flask.ext.script import Manager, prompt, prompt_choices #@UnresolvedImport
+from flask.ext.script import Manager, Command, prompt, prompt_choices, prompt_bool #@UnresolvedImport
 
 from adsabs import create_app
 from config import config
 
 config.LOGGING_CONFIG = None
-logging.basicConfig(format='%(levelname)s - %(message)s', level=logging.INFO)
+logging.basicConfig(format='%(message)s', level=logging.INFO)
 
 app = create_app(config)
-manager = Manager(app)
+manager = Manager(app, with_default_commands=False)
 
 log = logging.getLogger("shell")
 
-@manager.command
-def run():
-    """Run server that can be reached from anywhere."""
-    app.run(host='0.0.0.0')
+tools_manager = Manager("Tools commands", with_default_commands=False)
 
-@manager.command
+@tools_manager.command
 def tools():
     """
     download and setup of extras in the ./tools directory
@@ -66,34 +63,10 @@ def tools():
     python manage.py runserver
     """
 
-@manager.command
-def create_api_user(email=None, level=None):
-    
-    from adsabs.modules.user import AdsUser
-    from adsabs.modules.api import user
-    
-    if not email:
-        email = prompt("Enter e-mail address of Classic ADS account")
-        if not email:
-            sys.exit(1)
-    if not level:
-        level = prompt_choices("Enter developer permission level", sorted(user.PERMISSION_LEVELS.keys()), "basic")
-        if level not in user.PERMISSION_LEVELS:
-            sys.exit(1)
-            
-    ads_user = AdsUser.from_email(email)
-    if not ads_user:
-        log.info("user not found")
-        sys.exit(1)
-        
-    # first check if the user is already a dev
-    api_user = user.AdsApiUser(ads_user.user_rec)
-    if api_user.is_developer():
-        log.info("User already has api access. Developer key: %s" % api_user.get_dev_key())
-    else:
-        api_user = user.create_api_user(ads_user, level)
-        dev_key = api_user.get_dev_key()
-        log.info("API User created with %s permissions. Developer key: %s" % (level, dev_key))
-    
+# register sub-managers
+from adsabs.modules.api import manager as api_manager
+manager.add_command('api', api_manager)
+manager.add_command('tools', tools_manager)
+
 if __name__ == "__main__":
     manager.run()
