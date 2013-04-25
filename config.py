1a518b2f79550983af566b4c70ecc04e97f43d5a
Jay Luker <lbjay@reallywow.com>
2013-04-25 16:38:56 -0300

    Added citation count to api response data

--- config.py
+++ config.py
@@ -139,7 +139,7 @@ class AppConfig(object):
     API_DEFAULT_RESPONSE_FORMAT = 'json'
     API_SOLR_DEFAULT_SORT = ('pubdate_sort','desc')
     
-    API_SOLR_DEFAULT_FIELDS = ['id','bibcode','title','author','pub','property','abstract','keyword',
+    API_SOLR_DEFAULT_FIELDS = ['id','bibcode','title','author','pub','property','abstract','keyword','citation_count',
                                'aff','database','identifier','doi','grants','year','issue','volume','page']
     API_SOLR_EXTRA_FIELDS = ['full','references','ack','score']
     API_SOLR_HIGHLIGHT_FIELDS = ['title','abstract','full','ack']
