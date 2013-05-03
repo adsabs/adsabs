65e0af4433891dbe8e3db0c4cbec2c3b19b031df
Giovanni Di Milia <giovanni@dimilia.it>
2013-05-03 17:34:51 -0400

    Few changes in the layout

--- config.py
+++ config.py
@@ -12,7 +12,7 @@ class AppConfig(object):
     #prints the template in the bottom of the page with the link to SOLR
     PRINT_DEBUG_TEMPLATE = False
     
-    APP_VERSION = '2013_05_01_v1'
+    APP_VERSION = '2013_05_03_v1'
     
     # Override in local_config.py, e.g. DEPLOYMENT_PATH = "/adsabs"
     DEPLOYMENT_PATH = None
