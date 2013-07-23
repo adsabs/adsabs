698a2bc6fe3b522f2b3c552f94c8608adb998b08
Jay Luker <jluker@cfa.harvard.edu>
2013-07-23 10:23:11 -0400

    app log error messages should include request path

--- adsabs/errors.py
+++ adsabs/errors.py
@@ -4,10 +4,10 @@ Created on Jan 7, 2013
 @author: jluker
 '''
 
-from flask import render_template, request, g, current_app as app
+from flask import render_template, request
 
 def create_error_handler(app, status_code, template):
     @app.errorhandler(status_code)
     def f(error):    
-        app.logger.error("error (%s): %s" % (status_code, str(error)))
+        app.logger.error("[error] %s, %s" % (str(error), request.path))
         return render_template(template), status_code
