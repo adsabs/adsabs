43c8037053432509fa7cb78ed64be1875dde04b1
Giovanni Di Milia <giovanni@dimilia.it>
2013-08-05 17:25:58 -0400

    Updated the user form to the length limits of ads classic

--- adsabs/modules/user/forms.py
+++ adsabs/modules/user/forms.py
@@ -5,8 +5,8 @@ from flask.ext.wtf import (Form, HiddenField, BooleanField, #@UnresolvedImport
 from flask.ext.wtf.html5 import EmailField #@UnresolvedImport
 
 class LoginForm(Form):
-    login = EmailField(u'Email address', [required(), length(min=5, max=2048), validators.Email()], description=u"Your login email")
-    password = PasswordField(u'Password', [required(), length(min=6, max=100)], description=u"Your password")
+    login = EmailField(u'Email address', [required(), length(min=1, max=2048), validators.Email()], description=u"Your login email")
+    password = PasswordField(u'Password', [required(), length(min=1, max=100)], description=u"Your password")
     remember = BooleanField('Remember me')
     next = HiddenField()
     submit = SubmitField('Login')
@@ -18,17 +18,28 @@ class ReauthForm(Form):
     
     
 class SignupForm(Form):
-    name = TextField(u'Name', [required(), length(min=1, max=2048)], description=u"Your name")
-    lastname = TextField(u'Last Name', [required(), length(min=1, max=2048)], description=u"Your last name")
-    login = EmailField(u'Email address', [required(), length(min=5, max=2048), validators.Email()], description=u"Your login email")
-    confirm_login = EmailField(u'Confirm Email address', [required(), length(min=5, max=2048), validators.Email(), validators.EqualTo('login', message='The two email fields must be equal')], description=u"Repeat your login email")
-    password = PasswordField(u'Password', [required(), length(min=6, max=100)], description=u"Your password")
-    confirm_password = PasswordField(u'Confirm Password', [required(), length(min=6, max=100), validators.EqualTo('password', message='The two password fields must be equal')], description=u"Repeat Your password")
+    name = TextField(u'Name', [length(min=1, max=30)], description=u"Your name")
+    lastname = TextField(u'Last Name', [required(), length(min=1, max=30)], description=u"Your last name")
+    login = EmailField(u'Email address', [required(), length(min=5, max=40), validators.Email()], description=u"Your login email")
+    confirm_login = EmailField(u'Confirm Email address', [required(), length(min=5, max=40), validators.Email(), validators.EqualTo('login', message='The two email fields must be equal')], description=u"Repeat your login email")
+    password = PasswordField(u'Password', [required(), length(min=6, max=8)], description=u"Your password")
+    confirm_password = PasswordField(u'Confirm Password', [required(), length(min=6, max=8), validators.EqualTo('password', message='The two password fields must be equal')], description=u"Repeat Your password")
     recaptcha = RecaptchaField()
     submit = SubmitField('Create user')
+    
+    def validate(self):
+        rv = Form.validate(self)
+        if not rv:
+            return False
+        if len(self.name.data) + len(self.lastname.data) > 29: #name plus lastname cannot be bigger than 29 (one char reserved for the separator)
+            self.name.errors.append('The name and the last name cannot be more than 29 chars long together (we will fix this, we promise)')
+            self.lastname.errors.append('The name and the last name cannot be more than 29 chars long together (we will fix this, we promise)')
+            return False
+        return True
+    
 
 class PreActivateUserForm(Form):
-    act_em = EmailField(u"Email", [required(), length(min=5, max=2048), validators.Email()], description=u"Your login email")
+    act_em = EmailField(u"Email", [required(), length(min=5, max=40), validators.Email()], description=u"Your login email")
     submit_act_email = SubmitField('Submit')
 
 class ActivateUserForm(Form):
@@ -36,30 +47,40 @@ class ActivateUserForm(Form):
     submit = SubmitField('Activate')
 
 class ActivateNewUsernameForm(ActivateUserForm):
-    password = PasswordField(u'Password to confirm changes', [required(), length(min=6, max=100)], description=u"Your password")
+    password = PasswordField(u'Password to confirm changes', [required(), length(min=6, max=8)], description=u"Your password")
 
 class ChangeUserParamsForm(Form):
-    name = TextField(u'Name', [required(), length(min=1, max=2048)], description=u"Your name")
-    lastname = TextField(u'Last Name', [required(), length(min=1, max=2048)], description=u"Your last name")
-    login = EmailField(u'Email address', [required(), length(min=5, max=2048), validators.Email()], description=u"Your login email")
-    confirm_login = EmailField(u'Confirm Email address', [required(), length(min=5, max=2048), validators.Email(), validators.EqualTo('login', message='The two email fields must be equal')], description=u"Repeat your login email")
-    password = PasswordField(u'Password to confirm changes', [required(), length(min=6, max=100)], description=u"Your password")
+    name = TextField(u'Name', [length(min=1, max=30)], description=u"Your name")
+    lastname = TextField(u'Last Name', [required(), length(min=1, max=30)], description=u"Your last name")
+    login = EmailField(u'Email address', [required(), length(min=5, max=40), validators.Email()], description=u"Your login email")
+    confirm_login = EmailField(u'Confirm Email address', [required(), length(min=5, max=40), validators.Email(), validators.EqualTo('login', message='The two email fields must be equal')], description=u"Repeat your login email")
+    password = PasswordField(u'Password to confirm changes', [required(), length(min=6, max=8)], description=u"Your password")
     submit = SubmitField('Modify settings')
+    
+    def validate(self):
+        rv = Form.validate(self)
+        if not rv:
+            return False
+        if len(self.name.data) + len(self.lastname.data) > 29: #name plus lastname cannot be bigger than 29 (one char reserved for the separator)
+            self.name.errors.append('The name and the last name cannot be more than 29 chars long together (we will fix this, we promise)')
+            self.lastname.errors.append('The name and the last name cannot be more than 29 chars long together (we will fix this, we promise)')
+            return False
+        return True
 
 class ResetPasswordForm(Form):
-    login = EmailField(u'Email address', [required(), length(min=5, max=2048), validators.Email()], description=u"Your login email")
+    login = EmailField(u'Email address', [required(), length(min=5, max=40), validators.Email()], description=u"Your login email")
     submit = SubmitField('Send temporary reset code')
 
 class ResetPasswordFormConf(ResetPasswordForm):
     resetcode = TextField(u'Reset Code', [required(), length(min=1, max=2048)], description=u"Past here your reset code")
-    new_password = PasswordField(u'New Password', [required(), length(min=6, max=100)], description=u"Your new password")
-    confirm_new_password = PasswordField(u'Confirm New Password', [required(), length(min=6, max=100), validators.EqualTo('new_password', message='The two password fields must be equal')], description=u"Repeat your new password")
+    new_password = PasswordField(u'New Password', [required(), length(min=6, max=8)], description=u"Your new password")
+    confirm_new_password = PasswordField(u'Confirm New Password', [required(), length(min=6, max=8), validators.EqualTo('new_password', message='The two password fields must be equal')], description=u"Repeat your new password")
     submit = SubmitField('Change Password')
 
 class ChangePasswordForm(Form):
-    old_password = PasswordField(u'Current Password', [required(), length(min=6, max=100)], description=u"Your current password")
-    new_password = PasswordField(u'New Password', [required(), length(min=6, max=100)], description=u"Your new password")
-    confirm_new_password = PasswordField(u'Confirm New Password', [required(), length(min=6, max=100), validators.EqualTo('new_password', message='The two password fields must be equal')], description=u"Repeat your new password")
+    old_password = PasswordField(u'Current Password', [required(), length(min=6, max=8)], description=u"Your current password")
+    new_password = PasswordField(u'New Password', [required(), length(min=6, max=8)], description=u"Your new password")
+    confirm_new_password = PasswordField(u'Confirm New Password', [required(), length(min=6, max=8), validators.EqualTo('new_password', message='The two password fields must be equal')], description=u"Repeat your new password")
     submit = SubmitField('Modify your password')
     
     def validate(self):
@@ -68,5 +89,6 @@ class ChangePasswordForm(Form):
             return False
         if self.old_password.data == self.new_password.data:
             self.new_password.errors.append('The new password must be different from the old one')
+            self.confirm_new_password.errors.append('The new password must be different from the old one')
             return False
         return True
