c209f5e1dab47f6077af9a330328ff5a04342177
Edwin Henneken <ehenneken@cfa.harvard.edu>
2013-08-07 10:11:25 -0400

    Fixed failing unittest for citation helper

--- tests/test_modules_bibutils.py
+++ tests/test_modules_bibutils.py
@@ -18,6 +18,7 @@ from config import config
 from test_utils import (AdsabsBaseTestCase)
 
 import adsabs.modules.bibutils.biblio_functions as bf
+import adsabs.modules.bibutils.utils as utils
 
 import requests
 
@@ -35,8 +36,8 @@ class TestCitationHelper(AdsabsBaseTestCase):
         import types
         with self.app.test_request_context('/bibutils'):
             self.app.preprocess_request()
-            cits = bf.get_citations(bibcodes=['1996ApJ...469..437S'])
-            self.assertEqual(type(cits),types.ListType)
+            cits = utils.get_citations(bibcodes=['1996ApJ...469..437S'])
+            self.assertEqual(type(cits),types.DictType)
             self.assertTrue(len(cits)>0)
 
     def test_reference_list(self):
@@ -53,25 +54,6 @@ class TestCitationHelper(AdsabsBaseTestCase):
             data = {'2314ADS..4305...27Q':{'title':u"An Artist's View of the Next Generation ADS Digital Library System",
                 'author':u'Quest,+'}}
             self.assertEqual(bf.get_meta_data(results=[('2314ADS..4305...27Q',1)]),data)
-
-    def _get_citations(*args, **kwargs):
-        return ['a','b','c','d']
-
-    def _get_references(*args, **kwargs):
-        return ['c','c','c','d','e']
-
-    def _get_meta_data(*args, **kwargs):
-        d = {}
-        for x in ['a','b','c','d','e']:
-            d[x] = {'title':x.upper(),'author':x.upper()}
-        return d
-
-    def test_suggestions(self):
-        bf.get_references = self._get_references
-        bf.get_citations  = self._get_citations
-        bf.get_meta_data  = self._get_meta_data
-        data = [{'author': 'C', 'bibcode': 'c', 'score': 4, 'title': 'C'}, {'author': 'D', 'bibcode': 'd', 'score': 2, 'title': 'D'}]
-        self.assertEqual(bf.get_suggestions(bibcodes=['foo']),data)
 
 if __name__ == '__main__':
     unittest2.main()
