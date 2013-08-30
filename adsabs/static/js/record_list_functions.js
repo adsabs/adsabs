a7f4c579b748ed1033749e48169ddcd3f7a1c24b
Edwin Henneken <ehenneken@gmail.com>
2013-08-30 08:59:50 -0400

    First commit: 'analyze' button

--- adsabs/static/js/record_list_functions.js
+++ adsabs/static/js/record_list_functions.js
@@ -118,3 +118,26 @@ ResultListManager.view_author_network = function()
 };
 
 
+/*
+ * Function to get Citation Helper results
+ */
+ResultListManager.citation_helper = function()
+{
+        //if I have checked bibcodes
+        if ($('#search_results_form').find('input[name="bibcode"]:checked').length > 0)
+        {
+            // Gather the checked bibcodes
+            ResultListManager.checked_bibcodes = new Array();
+            var $inputs = $('#search_results_form').find('input[name="bibcode"]:checked');
+            $inputs.each(function() {
+                ResultListManager.checked_bibcodes.push($(this).attr('value'));
+            });
+            // Call the Citation Helper with these bibcodes
+        if ($('#search_results_form').find('input[name="bibcode"]:checked').length > 0)
+        {
+                $('#search_results_form').attr('bibcodes', ResultListManager.checked_bibcodes.join("\n")).attr('action', 'http://jadzia:5000/bibutils/citation_helper').submit();
+        }
+            // Display the results
+
+        }
+};
