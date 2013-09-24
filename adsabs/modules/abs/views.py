ad37c4ee31809113e0bbe1418dd5bbceb38493d8
Jay Luker <jluker@cfa.harvard.edu>
2013-09-24 11:15:41 -0400

    trailing slash in url route causes redirect to proxied port

--- adsabs/modules/abs/views.py
+++ adsabs/modules/abs/views.py
@@ -35,7 +35,7 @@ def add_caching_header(response):
     response.headers.setdefault('Cache-Control', cache_header)
     return response
 
-@abs_blueprint.route('/<bibcode>/', methods=['GET'])
+@abs_blueprint.route('/<bibcode>', methods=['GET'])
 def abstract(bibcode):
     
     solrdoc = get_document(bibcode)
@@ -48,7 +48,7 @@ def abstract(bibcode):
     
     return render_template('abstract_tabs.html', solrdoc=solrdoc, inveniodoc=inveniodoc, curview='abstract')
     
-@abs_blueprint.route('/<bibcode>/<list_type>/', methods=['GET'])
+@abs_blueprint.route('/<bibcode>/<list_type>', methods=['GET'])
 def tab_list(bibcode, list_type):
 
     #I get the document
