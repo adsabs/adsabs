a24dd3a8d1864aeeef86f4fb968cc70a43ff3ab8
Roman Chyla <roman.chyla@gmail.com>
2013-12-13 11:10:55 -0500

    Added the backend part of the bitset gateway; the GUI should be simple, but it isn't - entering brain hibernation...

--- adsabs/modules/search/views.py
+++ adsabs/modules/search/views.py
@@ -1,14 +1,18 @@
 import sys
-from flask import Blueprint, request, g, render_template, flash, current_app, abort
+from flask import Blueprint, request, g, render_template, flash, current_app, abort, url_for
 from flask.ext.solrquery import solr #@UnresolvedImport
 
 #from flask.ext.login import current_user #@UnresolvedImport
 from .forms import QueryForm
+from .models import BigQuery
 from adsabs.core.solr import QueryBuilderSearch, AdsabsSolrqueryException
 from adsabs.core.data_formatter import field_to_json
 from config import config
 from adsabs.core.logevent import log_event
 import traceback
+import uuid
+import pytz
+from datetime import datetime
 
 #Definition of the blueprint
 search_blueprint = Blueprint('search', __name__, template_folder="templates", 
@@ -67,6 +71,65 @@ def search():
             for field_name, errors_list in form.errors.iteritems():
                 flash('errors in the form validation: %s.' % '; '.join(errors_list), 'error')
     return render_template('search.html', form=form)
+
+
+@search_blueprint.route('/bigquery/', methods=('GET', 'POST'))
+def bigquery():
+    """
+    Allows one to post a large number of ID's and get
+    results in the search form
+    
+    I need to get this to the form; the stupid thing is not sending
+    post data; or flask is not reading them; i don't know...
+    
+    $('div#bigquery');
+    $('div#footer').append($('<div id="bigquery"><form><div>Copy&Paste bibcodes here, one per line.</div><textarea type="hidden" name="bigquery" class="ajaxHiddenField" rows="30" cols="40">sfsdfdsf</textarea></form></div>'));
+    var big = $('div#bigquery');
+    big.append($('<input type="submit" value="Submit">').click(function() {
+        $.ajax({
+                type : "POST",
+                cache : false,
+                url : '/search/bigquery/',
+                data : big.serialize(),
+                contentType: 'plain/text',
+                processData: false,
+                success: function(data) {
+                    $.fancybox.hideLoading();
+                    //add query id to the form
+                    $('#q').val($('#q').val() + data);
+                    //append an hidden parameter for the bibcodes retrieved
+                    //submit the form
+                    //$('form.form-search').submit();
+                }
+            });
+    }));
+    
+    $.fancybox.showLoading()
+    $.fancybox($('div#bigquery'))
+    
+    """
+    # just for debugging, return what we know about the query
+    if ('uuid' in request.values):
+        cursor = BigQuery.query.filter(BigQuery.query_id==request.values['uuid']) #@UndefinedVariable
+        query = cursor.first()
+        return query.data
+    
+    # receive the data from the form
+    v = request.values.get('bigquery')
+    if (v is None or len(v) == 0):
+        return ""
+    
+    # save data inside mongo and get unique id
+    qid = str(uuid.uuid4())
+    new_rec = BigQuery(query_id=qid,
+                created=datetime.utcnow().replace(tzinfo=pytz.utc),
+                data=v)
+    new_rec.save()
+    
+    # return the unique queryid
+    return qid
+    
+    
 
 @search_blueprint.route('/facets', methods=('GET', 'POST'))
 def facets():
