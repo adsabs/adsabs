4a4528afd289d74730811ea9de56651f9b8d569e
Edwin Henneken <ehenneken@cfa.harvard.edu>
2014-01-15 12:14:07 -0500

    Unit test with mock data for Citation Helper

--- tests/test_modules_bibutils.py
+++ tests/test_modules_bibutils.py
@@ -18,10 +18,9 @@ else:
 #from adsabs.app import create_app
 #from adsabs.core import solr
 from config import config
-from test_utils import (AdsabsBaseTestCase)
+from test_utils import (AdsabsBaseTestCase, ReferenceDataFixture, CitationDataFixture, MetaDataFixture)
 
-import adsabs.modules.bibutils.biblio_functions as bf
-import adsabs.modules.bibutils.utils as utils
+from adsabs.modules.bibutils import biblio_functions
 
 import requests
 
@@ -33,13 +32,13 @@ class TestCitationHelper(AdsabsBaseTestCase):
     def test_empty_input(self):
         with self.app.test_request_context('/bibutils'):
             self.app.preprocess_request()
-            self.assertEqual(bf.get_suggestions(), [])
+            self.assertEqual(biblio_functions.get_suggestions(), [])
 
     def test_reference_list(self):
         import types
         with self.app.test_request_context('/bibutils'):
             self.app.preprocess_request()
-            refs = bf.get_references(bibcodes=['1996ApJ...469..437S'])
+            refs = biblio_functions.get_references(bibcodes=['1996ApJ...469..437S'])
             self.assertEqual(type(refs),types.ListType)
             self.assertTrue(len(refs)>0)
 
@@ -48,7 +47,16 @@ class TestCitationHelper(AdsabsBaseTestCase):
             self.app.preprocess_request()
             data = {'2314ADS..4305...27Q':{'title':u"An Artist's View of the Next Generation ADS Digital Library System",
                 'author':u'Quest,+'}}
-            self.assertEqual(bf.get_meta_data(results=[('2314ADS..4305...27Q',1)]),data)
+            self.assertEqual(biblio_functions.get_meta_data(results=[('2314ADS..4305...27Q',1)]),data)
+
+    def test_citation_helper(self):
+        with self.app.test_request_context('/bibutils'):
+            expected = [{'author': 'x_author', 'bibcode': 'x', 'score': 2, 'title': 'x_title'}, 
+                        {'author': 'z_author', 'bibcode': 'z', 'score': 2, 'title': 'z_title'}]
+            self.useFixture(ReferenceDataFixture())
+            self.useFixture(CitationDataFixture())
+            self.useFixture(MetaDataFixture())
+            self.assertEqual(biblio_functions.get_suggestions(bibcodes=['a','b','c']),expected)
 
 if __name__ == '__main__':
     unittest.main()
