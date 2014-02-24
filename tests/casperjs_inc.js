94ccfc42375d6f30be1e9f6243ebdc8b7505b74c
Jay Luker <jluker@cfa.harvard.edu>
2014-02-24 09:09:58 -0500

    make stepTimeout a cmd line arg; tweak to tour step on abstract page to avoid test errors when recommendations aren't available

--- tests/casperjs_inc.js
+++ tests/casperjs_inc.js
@@ -6,4 +6,9 @@ casper.baseTestUrl = casper.cli.has("baseTestUrl")
     ? casper.cli.get("baseTestUrl")
     : "http://localhost:5000";
 
-casper.options.stepTimeout = 5000;
+casper.options.stepTimeout = casper.cli.has('stepTimeout')
+	? casper.cli.options.stepTimeout
+	: 5000;
+
+casper.echo('baseTestUrl: ' + casper.baseTestUrl);
+casper.echo('stepTimeout: ' + casper.options.stepTimeout);
