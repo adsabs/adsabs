e86e88968a9a7b00b02474fb6801edfffd5b463b
Vladimir Sudilovsky <vsudilovsky@gmail.com>
2014-03-03 15:54:22 -0500

    Change entrypoint script name; remove extraneous sys.path manipulations

--- gunicorn.conf.py
+++ gunicorn.conf.py
@@ -1,10 +1,11 @@
 import multiprocessing
 
-bind = "unix://tmp/gunicorn.socket"
+#bind = "unix://tmp/gunicorn.socket"
+bind = "127.0.0.1:8000"
 workers = multiprocessing.cpu_count() * 2 + 1
-max_requests = 100
+max_requests = 100 #maximum number of requests before a worker is restarted
 preload_app = True
 chdir = '/vagrant/adsabs/'
-daemon = True
-debug = False
+daemon = False
+debug = True
 errorlog = '/tmp/gunicorn.error.log'
