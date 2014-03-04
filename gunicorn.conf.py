715a27089c20757b46677abe8e347d15039a204e
Vladimir Sudilovsky <vsudilovsky@gmail.com>
2014-03-04 17:32:49 -0500

    Production settings in gunicorn.conf.py

--- gunicorn.conf.py
+++ gunicorn.conf.py
@@ -1,11 +1,12 @@
 import multiprocessing
 
-#bind = "unix://tmp/gunicorn.socket"
-bind = "127.0.0.1:8000"
+bind = "unix:///tmp/gunicorn.socket"
+#bind = "127.0.0.1:8000"
 workers = multiprocessing.cpu_count() * 2 + 1
 max_requests = 100 #maximum number of requests before a worker is restarted
 preload_app = True
 chdir = '/vagrant/adsabs/'
-daemon = False
-debug = True
+daemon = True
+debug = False
 errorlog = '/tmp/gunicorn.error.log'
+pidfile = '/tmp/gunicorn.pid'
