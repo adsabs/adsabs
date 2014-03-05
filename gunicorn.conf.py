6646fcaa0118445bd98a9c35feb7460a2d2568f3
Vladimir Sudilovsky <vsudilovsky@gmail.com>
2014-03-05 09:34:19 -0500

    gunicorn chdir is now dirname(__file__)

--- gunicorn.conf.py
+++ gunicorn.conf.py
@@ -1,11 +1,12 @@
 import multiprocessing
+import os
 
 bind = "unix:///tmp/gunicorn.socket"
 #bind = "127.0.0.1:8000"
 workers = multiprocessing.cpu_count() * 2 + 1
 max_requests = 100 #maximum number of requests before a worker is restarted
 preload_app = True
-chdir = '/vagrant/adsabs/'
+chdir = os.path.dirname(__file__)
 daemon = True
 debug = False
 errorlog = '/tmp/gunicorn.error.log'
