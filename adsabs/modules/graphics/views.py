e6da09b8987fa64008726a15364053656cc417a6
Edwin Henneken <ehenneken@gmail.com>
2014-09-17 11:03:03 -0400

    added JSON output to export image info

--- adsabs/modules/graphics/views.py
+++ adsabs/modules/graphics/views.py
@@ -7,7 +7,6 @@ from flask import (Blueprint, request, url_for, Response, current_app as app, ab
 #from flask.ext.solrquery import solr, signals as solr_signals #@UnresovledImport
 from config import config
 from paper_thumbnails import get_thumbnails
-from adsabs.extensions import statsd
 import ptree
 
 graphics_blueprint = Blueprint('graphics', __name__, template_folder="templates", url_prefix='/graphics')
@@ -30,8 +29,9 @@ def display_image(bibcode,figure_id,image_format):
     header = {'Content-type': 'image/%s'%image_ext}
     return image_data, 200, header
 
-@graphics_blueprint.route('/thumbnails/<bibcode>', methods=['GET', 'POST'])
-def thumbnails(bibcode):
+@graphics_blueprint.route('/thumbnails/<bibcode>', defaults={'format': None}, methods=['GET', 'POST'])
+@graphics_blueprint.route('/thumbnails/<bibcode>/<format>', methods=['GET', 'POST'])
+def thumbnails(bibcode,format):
     """
     View that creates overview of thumbnails with article graphics
     """
@@ -41,5 +41,8 @@ def thumbnails(bibcode):
     except Exception,e:
         app.logger.error('ID %s. Unable to get thumbnails for bibcode : %s! (%s)' % (g.user_cookie_id,bibcode,e))
         return render_template('recommendations_embedded.html', results={})
-
-    return render_template('thumbnails_embedded.html', results=results)
+    print results['figures']
+    if format == 'json':
+        return jsonify(paper=bibcode, graphics=results)
+    else:
+        return render_template('thumbnails_embedded.html', display=results)
